6218549abae408d7277e28daecec844523ead430
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-08-08 11:55:34 +0700

    03 - Product Advance Setup

--- CJK.Web/Startup.cs
+++ CJK.Web/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CJK.Web.Services;
+using CJK.Web.Services.IServices;
 
 namespace CJK.Web
 {
@@ -23,6 +25,10 @@ namespace CJK.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<IProductService, ProductService>();
+            SD.ProductAPIBase = Configuration["ServiceUrls:ProductAPI"];
+
+            services.AddScoped<IProductService, ProductService>();
             services.AddControllersWithViews();
         }
 
