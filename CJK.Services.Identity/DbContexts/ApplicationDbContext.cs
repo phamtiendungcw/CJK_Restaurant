1c1d1e60c2c2f962e596c668b657c8c3eab0ed72
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-08-10 16:30:44 +0700

    Configuring APIScope Resource and Clients for Identity Server

---
+++ CJK.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CJK.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CJK.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ CJK.Services.Identity/SD.cs
@@ -0,0 +1,40 @@
+﻿using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+
+namespace CJK.Services.Identity
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("CJK", "CJK Server"),
+                new ApiScope("read","Read your data"),
+                new ApiScope("write","Write your data"),
+                new ApiScope("delete","Delete your data")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client()
+                {
+                    ClientId = "client",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read","write","profile"}
+                },
+            };
+    }
+}
